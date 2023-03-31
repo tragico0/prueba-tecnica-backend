2d49247f12508439802ab729d85ea67d412fe226
Victor Martinez <vic.mtz96@hotmail.com>
2023-03-30 18:12:20 -0600

    Add route to create employee

---
+++ routes/v1/employees/create-employee.ts
@@ -0,0 +1,49 @@
+import { plainToInstance } from "class-transformer";
+import { Request, Response } from "express";
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+import { isEmpty } from "lodash";
+import db from '../../../database';
+import { toEntity } from "../../../database/utils";
+import { Employee, RoleCode } from "../../../models/employee";
+
+interface CreateEmployeeData {
+    reference: string;
+    firstName: string;
+    firstLastName: string;
+    hourlyRate: number;
+    roleCode: RoleCode;
+}
+
+export default async (req: Request, res: Response) => {
+    if (isEmpty(req.body?.data)) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            message: ReasonPhrases.BAD_REQUEST
+        });
+    }
+
+    try {
+        const employee = await createEmployee(req.body.data);
+
+        res.status(200).json({
+            data: toEntity(Employee, employee)
+        });
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            message: ReasonPhrases.INTERNAL_SERVER_ERROR
+        });
+    }
+}
+
+async function createEmployee (data: CreateEmployeeData) {
+    const { rows: [result] } = await db.query(`
+        SELECT * from create_employee($1, $2, $3, $4, $5);
+    `, [
+        data.reference,
+        data.firstName,
+        data.firstLastName,
+        data.hourlyRate,
+        data.roleCode
+    ]);
+
+    return toEntity(Employee, result);
+}
--- routes/v1/employees/index.ts
+++ routes/v1/employees/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import createEmployee from './create-employee';
 import listEmployees from './list-employees';
 
 const router = Router();
 
 router.get('/', listEmployees)
+router.post('/create', createEmployee);
 
 export default router;
