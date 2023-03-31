7fe50903f1b3c1edad019a3b742d7aa42584f33a
Victor Martinez <vic.mtz96@hotmail.com>
2023-03-31 10:14:06 -0600

    Add grocery vouchers to tax seed

--- migrations/1680212655511_initial-tax-records.ts
+++ migrations/1680212655511_initial-tax-records.ts
@@ -10,7 +10,8 @@ export async function up (pgm: MigrationBuilder): Promise<void> {
         (label, code, percentage, amount, disabled)
         VALUES
         ('ISR', 'ISR_9', 9.0, null, false),
-        ('ISR > $10,000', 'ISR_GT_10000', 3.0, null, false)
+        ('ISR > $10,000', 'ISR_GT_10000', 3.0, null, false),
+        ('Vales de despensa', 'GROCERY_VOUCHERS_4', 4, null, false)
         ;
     `);
 }
@@ -18,6 +19,6 @@ export async function up (pgm: MigrationBuilder): Promise<void> {
 export async function down (pgm: MigrationBuilder): Promise<void> {
     pgm.db.query(`
         DELETE FROM ${TableName.TAXES}
-        WHERE code IN ('ISR_9', 'ISR_GT_10000');
+        WHERE code IN ('ISR_9', 'ISR_GT_10000', 'GROCERY_VOUCHERS_4');
     `);
 }
