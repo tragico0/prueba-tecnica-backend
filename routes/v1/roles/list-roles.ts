970368e43b64d2b864335b77af2451c489dbd216
Victor Martinez <vic.mtz96@hotmail.com>
2023-03-31 11:08:23 -0600

    Get roles from table

--- routes/v1/roles/list-roles.ts
+++ routes/v1/roles/list-roles.ts
@@ -1,9 +1,28 @@
 import { Request, Response } from 'express';
-import { values } from 'lodash';
-import { RoleCode } from '../../../models/employee';
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+import { map } from 'lodash';
+import db, { TableName } from '../../../database';
+import { toEntity } from '../../../database/utils';
+import { Role } from '../../../models/role';
 
 export default async (req: Request, res: Response) => {
-    res.status(200).json({
-        data: values(RoleCode)
-    });
+    try {
+        const roles = await getRoles();
+
+        res.status(200).json({
+            data: map(roles, (employee) => toEntity(Role, employee))
+        });
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            message: ReasonPhrases.INTERNAL_SERVER_ERROR
+        });
+    }
+}
+
+async function getRoles () {
+    const { rows } = await db.query(`
+        SELECT * FROM ${TableName.ROLES}
+    `);
+
+    return rows;
 }
