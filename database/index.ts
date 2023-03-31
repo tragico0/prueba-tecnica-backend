cf5905a3847baf5c5d7f639a3345749c364853d9
Victor Martinez <vic.mtz96@hotmail.com>
2023-03-30 18:20:59 -0600

    Fix missing table names

--- database/index.ts
+++ database/index.ts
@@ -1,3 +1,11 @@
 import databaseConnection from './connection';
 
+export enum TableName {
+    TAXES = 'taxes',
+    BONUS = 'bonus',
+    EMPLOYEES = 'employees',
+    PAYROLL_COVER = 'payroll_cover',
+    PAYROLL_ROW = 'payroll_row'
+}
+
 export default databaseConnection;
