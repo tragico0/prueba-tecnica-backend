d188ce20df642b9a8cdee59a147aea26d5d6e58a
Victor Martinez <vic.mtz96@hotmail.com>
2023-04-01 02:54:20 -0600

    Update employee deliveries by reference, name or role code

--- routes/v1/employees/add-employee-deliveries.ts
+++ routes/v1/employees/add-employee-deliveries.ts
@@ -1,13 +1,13 @@
+import BPromise from 'bluebird';
 import { Request, Response } from "express";
-import { head, isEmpty, isNil } from "lodash";
-import { StatusCodes, ReasonPhrases } from 'http-status-codes';
-import db, { TableName } from '../../../database';
-import moment, { Moment } from 'moment-timezone';
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+import { head, isEmpty, isNil, map, reduce } from "lodash";
+import moment from 'moment-timezone';
 import Config from '../../../config';
-import { Employee } from "../../../models/employee";
+import db, { TableName } from '../../../database';
 import { toEntity } from "../../../database/utils";
+import { Employee } from "../../../models/employee";
 import { PayrollCover } from "../../../models/payroll-cover";
-import { plainToClassFromExist } from "class-transformer";
 
 export default async (req: Request, res: Response) => {
     if (isEmpty(req.body?.data)) {
@@ -25,14 +25,6 @@ export default async (req: Request, res: Response) => {
     } = req.body.data;
 
     try {
-        const employee = await findEmployeeByReference(employeeReference);
-
-        if (isNil(employee)) {
-            return res.status(StatusCodes.BAD_REQUEST).json({
-                message: ReasonPhrases.BAD_REQUEST
-            });
-        }
-
         const monthIndex = (selectedMonth - 1);
         const targetMonth = moment.utc().tz(Config.defaultTimezone).month(monthIndex);
         const currentMonth = moment.utc().tz(Config.defaultTimezone);
@@ -43,16 +35,37 @@ export default async (req: Request, res: Response) => {
             });
         }
 
-        const [payrollCover] = await findOrCreatePayrollCover(employee, targetMonth, deliveriesQuantity);
-        const updatedPayrollCover = await addDeliveriesToPayrollCover(
-            payrollCover.id,
+        const startOfMonth = targetMonth.clone().startOf('month').toISOString();
+        const endOfMonth = targetMonth.clone().endOf('month').toISOString();
+        let employees: Employee[] = [];
+        
+        if (!isEmpty(employeeReference)) {
+            const result = await findEmployeeByReference(employeeReference);
+            employees = isNil(result) ? [] : [result];
+        } else if (!isEmpty(employeeName)) {
+            const result = await findEmployeeByName(employeeName);
+            employees = isNil(result) ? [] : [result];
+        } else if (!isEmpty(employeeRole)) {
+            employees = await findEmployeesByRoleCode(employeeRole);
+        } else {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                message: ReasonPhrases.BAD_REQUEST
+            });
+        }
+
+        const payrollCovers = await BPromise.mapSeries(employees, async (employee) => {
+            const [payrollCover] = await findOrCreatePayrollCoverByEmployeeId(employee, startOfMonth, endOfMonth, deliveriesQuantity);
+            return payrollCover;
+        });
+
+        await addDeliveriesToPayrollCover(
+            map(payrollCovers, (payrollCover) => payrollCover.id),
             deliveriesQuantity
         );
 
-        return res.status(200).json({     
-            data: plainToClassFromExist(payrollCover, updatedPayrollCover)
+        return res.status(200).json({
+            data: true
         });
-
     } catch (error) {
         console.error(error);
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
@@ -61,29 +74,18 @@ export default async (req: Request, res: Response) => {
     }
 }
 
-async function findEmployeeByReference (employeeReference: string) {
-    const { rows: [employee] } = await db.query(`
-        SELECT * FROM ${TableName.EMPLOYEES}
-        WHERE reference = $1
-    `, [employeeReference])
-
-    return toEntity(Employee, employee);
-}
-
-async function findOrCreatePayrollCover (
+async function findOrCreatePayrollCoverByEmployeeId (
     employee: Employee,
-    selectedMonth: Moment,
+    startOfMonth: string,
+    endOfMonth: string,
     deliveriesQuantity: number
 ): Promise<[PayrollCover, boolean]> {
-    const startOfMonth = selectedMonth.clone().startOf('month');
-    const endOfMonth = selectedMonth.clone().endOf('month');
-
     let { rows: [payrollCover] } = await db.query(`
         SELECT * FROM ${TableName.PAYROLL_COVER}
         WHERE employee_id = $1
             AND created_at BETWEEN $2 AND $3;
     `, [
-        employee.id, startOfMonth.toISOString(), endOfMonth.toISOString()
+        employee.id, startOfMonth, endOfMonth
     ]);
 
     if (isNil(payrollCover)) {
@@ -99,15 +101,60 @@ async function findOrCreatePayrollCover (
     return [toEntity(PayrollCover, payrollCover), false];
 }
 
-async function addDeliveriesToPayrollCover (payrollCoverId: number, quantity: number): Promise<Partial<PayrollCover>> {
-    const { rows: [result] } = await db.query(`
-        UPDATE ${TableName.PAYROLL_COVER}
-        SET deliveries_count = deliveries_count + $1
-        WHERE id = $2
-        RETURNING deliveries_count
+async function findEmployeeByReference (employeeReference: string) {
+    const { rows: [employee] } = await db.query(`
+        SELECT * FROM ${TableName.EMPLOYEES}
+        WHERE reference = $1
+    `, [employeeReference])
+
+    return toEntity(Employee, employee);
+}
+
+async function findEmployeeByName (employeeName: string) {
+    const { rows: [employee] } = await db.query(`
+        SELECT e.*
+        FROM ${TableName.EMPLOYEES} e
+        WHERE CONCAT(e.first_name, e.first_last_name) = $1
+    `, [employeeName]);
+
+    return toEntity(Employee, employee);
+}
+
+async function findEmployeesByRoleCode (roleCode: string) {
+    const { rows } = await db.query(`
+        SELECT e.*
+        FROM ${TableName.EMPLOYEES} e
+        INNER JOIN ${TableName.ROLES} r ON (r.id = e.role_id AND r.deleted_at IS NULL)
+        WHERE e.deleted_at IS NULL
+            AND r.code = $1
+    `, [roleCode]);
+
+    return isEmpty(rows) ? [] : map(rows, (row) => {
+        return toEntity(Employee, row)
+    });
+}
+
+async function addDeliveriesToPayrollCover (payrollCoverIds: number[], quantity: number): Promise<any> {
+    if (isEmpty(payrollCoverIds)) {
+        return;
+    }
+
+    let parametersCount = 2;
+    const placeholders = reduce(payrollCoverIds, (acc: string, _, index: number) => {
+        acc += `$${parametersCount}` + (index < (payrollCoverIds.length - 1) ? ',' : '');
+        parametersCount += 1;
+        return acc;
+    }, '');
+
+    const { rows } = await db.query(`
+        UPDATE ${TableName.PAYROLL_COVER} pc
+            SET deliveries_count = deliveries_count + $1
+        FROM ${TableName.EMPLOYEES} e
+        WHERE e.id = pc.employee_id AND e.deleted_at IS NULL AND pc.deleted_at IS NULL
+            AND pc.id IN (${placeholders})
     `, [
-        quantity, payrollCoverId
+        quantity, ...payrollCoverIds
     ]);
 
-    return toEntity(PayrollCover, result);
+    return;
 }
